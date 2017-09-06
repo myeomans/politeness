ce7d83cd2cf4980dba796660bb74e52bbe459367
AlejandroKantor <akantorb@gmail.com>
2017-09-06 17:36:12 -0400

    Add data documentation

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Purchase offers for phone
+#'
+#' A dataset containing the purchase offer message and a labeling indicating if message
+#' is warm or tough
+#' @format A data frame with 355 rows and 2 variables:
+#' \describe{
+#'   \item{message}{character of purchase offer message}
+#'   \item{condition}{character label indicating if message is warm or tough}
+#' }
+#' @source \url{http:/}
+"phone_offers"
+
