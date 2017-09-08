881ab25854e35179e36f803f338df1235e1327e5
myeomans <mk.yeomans@gmail.com>
2017-09-08 00:42:47 -0400

    catching up

--- R/data.R
+++ R/data.R
@@ -1,12 +1,50 @@
+#' Positive Emotions List
+#'
+#' Positive words.
+#'
+#' @format A list of 2006 positively-valenced words
+#'
+"positive_list"
+
+#' Negative Emotions List
+#'
+#' Negative words.
+#'
+#' @format A list of 4783 negatively-valenced words
+#'
+"negative_list"
+
+#' Hedge Words List
+#'
+#' Hedges
+#'
+#' @format A list of 99 hedging words.
+#'
+"hedge_list"
+
+#' Feature Dictionaries
+#'
+#' Six dictionary-like features for the detector: Negations; Pauses; Swearing; Pronouns; Formal Titles; and Informal Titles.
+#'
+#' @format A list of six \code{quanteda::dictionary} objects
+"polite_dicts"
+
 #' Purchase offers for phone
 #'
-#' A dataset containing the purchase offer message and a labeling indicating if message
-#' is warm or tough
+#' A dataset containing the purchase offer message and a
+#' labeling indicating if message is warm or tough
 #' @format A data frame with 355 rows and 2 variables:
 #' \describe{
 #'   \item{message}{character of purchase offer message}
 #'   \item{condition}{character label indicating if message is warm or tough}
 #' }
-#' @source \url{http:/}
+#' @source \url{https://osf.io/t7sd6/}
 "phone_offers"
 
+
+#' Table of Politeness Features
+#'
+#' This table describes all the text features extracted in this package.
+#' Meant for the vignette.
+#'
+"feature_table"
