b15c28952d0bd20c6bfbaab4f52b3aad70332c9e
AlejandroKantor <akantorb@gmail.com>
2017-09-15 16:54:58 -0400

    New tests

--- tests/testthat/test-politeness.R
+++ tests/testthat/test-politeness.R
@@ -9,13 +9,13 @@ num_messages <- length(phone_offers$message)
 df_polite_bin_drop <- politeness(text = phone_offers$message, parser = "none", binary = TRUE, drop_blank = TRUE)
 
 # binary false, drop_blank true
-df_polite_bin_f_drop <- politeness(text = phone_offers$message, parser = "spacy", binary = FALSE, drop_blank = TRUE)
+df_polite_bin_f_drop <- politeness(text = phone_offers$message, parser = "none", binary = FALSE, drop_blank = TRUE)
 
 # binary false, drop_blank false
-df_polite_bin_f_drop_f <- politeness(text = phone_offers$message, parser = "spacy", binary = FALSE, drop_blank = FALSE)
+df_polite_bin_f_drop_f <- politeness(text = phone_offers$message, parser = "none", binary = FALSE, drop_blank = FALSE)
 
 # binary true, drop_blank false
-df_polite_bin_drop_f <- politeness(text = phone_offers$message, parser = "spacy", binary = TRUE, drop_blank = FALSE)
+df_polite_bin_drop_f <- politeness(text = phone_offers$message, parser = "none", binary = TRUE, drop_blank = FALSE)
 
 test_that("runs with parser none", {
   expect_equal(nrow(df_polite_bin_drop), num_messages)
@@ -31,25 +31,34 @@ test_that("runs with parser none", {
   expect_false(setequal(unique(unlist(df_polite_bin_f_drop)),0:1))
 })
 
+test_that("empty or na string", {
+ phone_offers$message[1] <- ""
 
-phone_offers$message[1] <- ""
+ df_polite <- politeness(text = c("","a"), parser = "none", binary = TRUE, drop_blank = FALSE)
+ expect_equal(apply(df_polite, MARGIN = 1, FUN  = sum), c(0,0))
 
-df_polite_bin_drop <- politeness(text = phone_offers$message, parser = "none", binary = TRUE, drop_blank = TRUE)
+ df_polite <- politeness(text = c(NA_character_,"a"), parser = "none", binary = TRUE, drop_blank = FALSE)
+ expect_equal(apply(df_polite, MARGIN = 1, FUN  = sum), c(0,0))
 
+ phone_offers$message[1] <- NA_character_
 
-#! should this through an error??
-phone_offers$message[1] <- NA_character_
-
-df_polite_bin_drop <- politeness(text = phone_offers$message, parser = "none", binary = TRUE, drop_blank = TRUE)
-
-
-# what if data has NA?
-#" what if fhta is empty"
-#
+ df_polite <- politeness(text = phone_offers$message, parser = "none", binary = TRUE, drop_blank = FALSE)
+ expect_equal(apply(df_polite, MARGIN = 1, FUN  = sum)[1], 0)
+})
 
 
-# spacy
-# binary true, drop.blank true
+test_that("factor", {
+  phone_offers$message[1] <- ""
+  phone_offers$message <- factor(phone_offers$message)
+  df_polite <- politeness(text = phone_offers$message, parser = "none", binary = TRUE, drop_blank = FALSE)
+  expect_true(any(apply(df_polite, MARGIN = 1, FUN  = sum) > 0))
+})
+
+test_that("text of length 0", {
+  df_polite <- politeness(text = phone_offers$message[2], parser = "none", binary = TRUE, drop_blank = FALSE)
+  expect_true(apply(df_polite, MARGIN = 1, FUN  = sum) > 0)
+})
+
 
 
 
--- tests/testthat/test-politenessPlot.R
+++ tests/testthat/test-politenessPlot.R
@@ -1,17 +1,9 @@
-
 # upload data
 
 data("phone_offers")
 
-
-
-
 # make politness data frames
 df_polite <- politeness(text = phone_offers$message, parser = "none", binary = TRUE)
-
-# has bug
-#df_polite_non_binary <- politeness(text = df_train$message, parser = "spacy", binary = FALSE)
-
 
 phone_offers$condition_factor <- factor(phone_offers$condition)
 levels(phone_offers$condition_factor) <- c("Cold","Warm")
@@ -19,46 +11,47 @@ levels(phone_offers$condition_factor) <- c("Cold","Warm")
 phone_offers$condition_character <- as.character(phone_offers$condition_factor)
 phone_offers$condition_integer <- as.integer(phone_offers$condition)
 phone_offers$condition_logical <- as.logical(phone_offers$condition)
-View(phone_offers)
 # test logical
-g_polite <- politenessPlot(df_polite,
-                           split = phone_offers[["condition_logical"]],
-                           split_levels = c("not polite","polite"),
-                           split_name =NULL,
-                           top_title = ""
-)
 
-print(g_polite)
+test_that("plot with different split variable" , {
+  expect_that({
+    politenessPlot(df_polite,
+                   split = phone_offers[["condition_logical"]],
+                   split_levels = c("not polite","polite"),
+                   split_name =NULL,
+                   top_title = ""
+    )
+  }, is_a("ggplot"))
+
+  expect_that({
+    politenessPlot(df_polite,
+                   split = phone_offers[["condition_factor"]],
+                   split_levels = NULL,
+                   split_name =NULL,
+                   top_title = ""
+    )
+  }, is_a("ggplot"))
+
+  expect_that({
+    politenessPlot(df_polite,
+                   split = phone_offers[["condition_character"]],
+                   split_levels = c("not polite","polite"),
+                   split_name =NULL,
+                   top_title = "title"
+    )
+  }, is_a("ggplot"))
 
 
-# test factor
-g_polite <- politenessPlot(df_polite,
-                           split = df_train[["warm_factor"]],
-                           split_levels = c("not polite","polite"),
-                           split_name =NULL,
-                           top_title = ""
-)
+  expect_that({
+    politenessPlot(df_polite,
+                   split = phone_offers[["condition_integer"]],
+                   split_levels = c("not polite","polite"),
+                   split_name =NULL,
+                   top_title = ""
+    )
+  }, is_a("ggplot"))
 
-print(g_polite)
-
-# test character
-g_polite <- politenessPlot(df_polite,
-                           split = df_train[["warm_character"]],
-                           split_name =NULL,
-                           top_title = ""
-)
-
-print(g_polite)
-
-# test integer
-g_polite <- politenessPlot(df_polite,
-                           split = df_train[["warm_integer"]],
-                           split_levels = c("not", ""),
-                           split_name =NULL,
-                           top_title = ""
-)
-
-print(g_polite)
+})
 
 
 
