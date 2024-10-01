b1ff82108c9d08f84ca54e193c4524d03b546acb
Michael Yeomans <myeomans@users.noreply.github.com>
2024-10-01 16:34:43 +0100

    x

--- R/modelPlot.R
+++ R/modelPlot.R
@@ -24,44 +24,44 @@ modelPlot<-function(model1,counts,model2=NULL,dat=FALSE){
     dplyr::rename(score=".") %>%
     dplyr::filter(score!=0 & feat!="(Intercept)" & !is.na(score))
 
-  if(nrow(plotCoefs)==0){
-    stop("Model must have non-zero coefficients")
-  }
-  plotDat<-plotCoefs %>%
-    dplyr::left_join(data.frame(feat=colnames(as.matrix(counts)),
-                         freq=colMeans(as.matrix(counts))))  %>%
-    dplyr::mutate_at(dplyr::vars(score,freq),~round(.,3))
-
-  if(sum(is.na(plotDat$freq))>0){
-    stop("Feature counts must be from same feature set used to train model")
-  }
-
-  xrange=range(plotDat$score)*1.2
-  yrange=c(.01,.05,.1,.2,.5,1,2,5,10,20,50)
-  yrange=yrange[yrange<max(colMeans(counts))]
-
-  thePlot<-plotDat %>%
-    ggplot2::ggplot(ggplot2::aes(x=score,y=freq,label=feat,color=score)) +
-    ggplot2::scale_color_gradient(low="navyblue",
-                                 high="forestgreen")+
-    ggplot2::geom_vline(xintercept=0)+
-    ggrepel::geom_label_repel(max.overlaps = 15,
-                              force_pull=3,
-                              force = 10)+
-    ggplot2::geom_point() +
-    ggplot2::scale_x_continuous(limits=xrange)+
-    ggplot2::scale_y_continuous(trans="log2",
-                               breaks=yrange)+
-    ggplot2::theme_bw() +
-    ggplot2::labs(x="Coefficient in Model",y="Uses per Document")+
-    ggplot2::theme(legend.position = "none",
-                  axis.title=ggplot2::element_text(size=20),
-                  axis.text=ggplot2::element_text(size=16))
-
-  if(dat){
-    return(list(plot=thePlot,
-                data=plotDat))
-  } else{
-    return(thePlot)
-  }
+  # if(nrow(plotCoefs)==0){
+  #   stop("Model must have non-zero coefficients")
+  # }
+  # plotDat<-plotCoefs %>%
+  #   dplyr::left_join(data.frame(feat=colnames(as.matrix(counts)),
+  #                        freq=colMeans(as.matrix(counts))))  %>%
+  #   dplyr::mutate_at(dplyr::vars(score,freq),~round(.,3))
+  #
+  # if(sum(is.na(plotDat$freq))>0){
+  #   stop("Feature counts must be from same feature set used to train model")
+  # }
+  #
+  # xrange=range(plotDat$score)*1.2
+  # yrange=c(.01,.05,.1,.2,.5,1,2,5,10,20,50)
+  # yrange=yrange[yrange<max(colMeans(counts))]
+  #
+  # thePlot<-plotDat %>%
+  #   ggplot2::ggplot(ggplot2::aes(x=score,y=freq,label=feat,color=score)) +
+  #   ggplot2::scale_color_gradient(low="navyblue",
+  #                                high="forestgreen")+
+  #   ggplot2::geom_vline(xintercept=0)+
+  #   ggrepel::geom_label_repel(max.overlaps = 15,
+  #                             force_pull=3,
+  #                             force = 10)+
+  #   ggplot2::geom_point() +
+  #   ggplot2::scale_x_continuous(limits=xrange)+
+  #   ggplot2::scale_y_continuous(trans="log2",
+  #                              breaks=yrange)+
+  #   ggplot2::theme_bw() +
+  #   ggplot2::labs(x="Coefficient in Model",y="Uses per Document")+
+  #   ggplot2::theme(legend.position = "none",
+  #                 axis.title=ggplot2::element_text(size=20),
+  #                 axis.text=ggplot2::element_text(size=16))
+  #
+  # if(dat){
+  #   return(list(plot=thePlot,
+  #               data=plotDat))
+  # } else{
+  #   return(thePlot)
+  # }
 }
